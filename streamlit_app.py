cd1c6c7e479445d4e730852fadc9bc3320335c22
nharrington-fortifieddata <nharrington@fortifieddata.com>
2022-12-30 19:39:13 -0600

    Update streamlit_app.py
    added normalization

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,3 +26,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# convets to human readable
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#displays results
+streamlit.dataframe(fruityvice_normalized)
